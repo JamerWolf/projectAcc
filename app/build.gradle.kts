b59065ff6284346fe51dda1fe24fc44ebb342471
Javier <javier@projectacc.dev>
2026-09-25 21:54:39 -0500

    chore: release 0.3.31 (versionCode 46)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.projectacc"
         minSdk = 24
         targetSdk = 36
-        versionCode = 45
-        versionName = "0.3.30"
+        versionCode = 46
+        versionName = "0.3.31"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
